da9e0354ccd8e6d2f0b0af866c69af9e37d91a63
Diego Alvarado <diego23k@gmail.com>
2018-01-26 13:45:18 +0100

    viernes manana

--- scripts/cvweb.js
+++ scripts/cvweb.js
@@ -1,1 +1,30 @@
-alert("connected");
+/* Smooth scroll */
+
+$('a[href*="#"]')
+  .click(function(event) {
+      var target = $(this.hash);
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top-60
+          },
+          1000,
+          function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      });
+
+
+  $('.nav').on('click',function(){
+    $('a.nav').removeClass('active');
+    $(this).addClass('active');
+  })
+
